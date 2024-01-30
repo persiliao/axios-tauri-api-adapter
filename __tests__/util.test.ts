069232da68567b7ad0f6ba7ec56c4b6cbfa78066
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2024-01-30 21:50:22 +0100

    test: Added tests for request data builder

--- __tests__/util.test.ts
+++ __tests__/util.test.ts
@@ -1,4 +1,5 @@
-import { buildRequestUrl } from './../src/util'
+import { buildRequestUrl, buildTauriRequestData } from './../src/util';
+import { TauriBodyType } from '../src/constants';
 
 test('only baseURL', () => {
   expect(buildRequestUrl({ baseURL: 'https://www.persiliao.com' })).toBe('https://www.persiliao.com')
@@ -18,4 +19,61 @@ test('only url with params & config.params', () => {
 
 test('only url & params', () => {
   expect(buildRequestUrl({ url: 'https://www.persiliao.com', params: { 'foo': 'bar' } })).toBe('https://www.persiliao.com?foo=bar')
+})
+
+
+test('data payload with correct type', () => {
+
+  // Tuples containing the value and its expected Tauri body type.
+  const payloads: [unknown, TauriBodyType | undefined][] = [
+    [
+      "banana!", 
+      TauriBodyType.TEXT
+    ],
+    [
+      { id: 1, name: "Banana", }, 
+      TauriBodyType.JSON
+    ],
+    [
+      new FormData, 
+      TauriBodyType.FORM
+    ],
+    [
+      new Uint32Array, 
+      TauriBodyType.BYTES
+    ],
+    [
+      new Float32Array, 
+      TauriBodyType.BYTES
+    ],
+    [
+      new BigInt64Array, 
+      TauriBodyType.BYTES
+    ],
+    [
+      [1, 2, 3], 
+      TauriBodyType.JSON
+    ],
+    [
+      ["THE WORLD"], 
+      TauriBodyType.JSON
+    ],
+    [
+      null, 
+      undefined
+    ],
+    [
+      undefined, 
+      undefined
+    ],
+    [
+      Symbol("foo"),
+      undefined,
+    ]
+  ];
+
+  for (const [value, expectedBodyType] of payloads) {
+    const body = buildTauriRequestData(value);
+    expect(body?.type).toBe(expectedBodyType);
+  }
 })
