b53ca0a1642ef1a96fc2032a790b1effe0e3e86d
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2024-01-30 10:17:00 +0100

    chore: Added some constants for next commits

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const TEXT_SERIALIZABLE_TYPES = new Set(["string", "boolean", "number", "bigint"]);
+
+export enum TauriBodyType {
+    TEXT = "Text",
+    JSON = "Json",
+    FORM = "Form",
+    BYTES = "Bytes",
+}
