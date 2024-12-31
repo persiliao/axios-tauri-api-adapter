9c2c0e4150974951e11558a5ad9db1a5ef00b056
Persi.Liao <xiangchu.liao@gmail.com>
2025-01-01 01:46:10 +0800

    test: Add demo

---
+++ demo/src-tauri/src/lib.rs
@@ -0,0 +1,25 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_webview_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
